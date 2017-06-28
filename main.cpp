1bc9a51691d8b9d9563652e4f278c59e3757711d
Catherine <cat.taylor.taylor@btinternet.com>
2017-06-28 14:45:44 +0100

    Began texture mapping.

--- main.cpp
+++ main.cpp
@@ -8,20 +8,16 @@
 #include <cstdio>
 #include <vector>
 #include "shader.hpp"
-#include "Read_Obj.hpp"
 #include "BITMAP.hpp"
+#include"Read_Obj.hpp"
 #include <glm/gtx/transform.hpp>
+
 
 int main(){
 
     unsigned char * texture_data;
 	int texture_width, texture_height;
-	texture_data = readBMP("sheet3.bmp", &texture_width, &texture_height);GLuint textureID;
-    glGenTextures(1, &textureID);
-    glBindTexture(GL_TEXTURE_2D, textureID);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, texture_width, texture_height, 0, GL_RGB, GL_UNSIGNED_BYTE, texture_data);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	texture_data = readBMP("sheet3.bmp", &texture_width, &texture_height);
 
     float *V, *N, *VT;
     int number_of_faces, *FV, *FN, *FT, number_of_vertices;
@@ -33,6 +29,7 @@ int main(){
         std::cout<<"Error: failed to initialize GLFW \n";
         return -1;
     }
+
     glfwWindowHint(GLFW_SAMPLES,4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
@@ -40,8 +37,8 @@ int main(){
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     GLFWwindow *window;
-    int width = 500, height = 500;
-    window = glfwCreateWindow(width, height, "Real-time Shadows", NULL, NULL);
+    int width =500, height = 500;
+    window = glfwCreateWindow(width, height, "Tutorial 01", NULL, NULL);
     if(window==NULL){
         std::cout<<"Error: failed to open window";
         glfwTerminate();
@@ -53,13 +50,92 @@ int main(){
         std::cout<<"Error: failed to initialize GLEW \n";
         return -1;
     }
-     do{        
+    
+    glm::mat4 ViewMatrix =  glm::lookAt(
+                            glm::vec3(0,0,-4), // position of camera
+                            glm::vec3(0,0,1),  // look at vector
+                            glm::vec3(0,1,0)  //look up vector
+    );
+
+    glm::mat4 ModelMatrix = glm::mat4(1.0f); //Create MVP matrices.
+    ModelMatrix[3].w = 1.0;
+    glm::mat4 projectionMatrix = glm::perspective(
+        glm::radians (90.0f),         //FOV
+        (float)width/(float)height, // Aspect Ratio. 
+        0.1f,        // Near clipping plane. 
+        100.0f       // Far clipping plane.
+    );
+    glm::mat4 MVP = projectionMatrix*ViewMatrix*ModelMatrix;
+    GLuint textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, texture_width, texture_height, 0, GL_RGB, GL_UNSIGNED_BYTE, texture_data);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+    GLuint VertexArrayID;
+    glGenVertexArrays(1, &VertexArrayID);
+    glBindVertexArray(VertexArrayID);
+
+    GLuint vertexbuffer;
+    glGenBuffers(1, &vertexbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    glBufferData(GL_ARRAY_BUFFER, 3*number_of_vertices*sizeof(float),  &V[0], GL_DYNAMIC_DRAW);
+
+    GLuint colorbuffer;
+    glGenBuffers(1, &colorbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+    glBufferData(GL_ARRAY_BUFFER, 2*number_of_vertices*sizeof(float),  &VT[0], GL_STATIC_DRAW);
+
+    GLuint programID = LoadShaders("VertexShader.vertexshader", "FragmentShader.fragmentshader");
+    GLuint MatrixID = glGetUniformLocation(programID, "MVP");
+
+    do{
+        
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+        glUseProgram(programID);
+        glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+
+        glEnable(GL_DEPTH_TEST);
+        glDepthFunc(GL_LESS);
+
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        glVertexAttribPointer(
+            0,
+            3,
+            GL_FLOAT,
+            GL_FALSE,
+            0,  
+            (void*)0
+        );
+        glEnableVertexAttribArray(1);
+        glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+        glVertexAttribPointer(
+            1,
+            2,
+            GL_FLOAT,
+            GL_FALSE,
+            0,  
+            (void*)0
+        );
+        glDrawArrays(GL_TRIANGLES, 0,3*number_of_faces);
+        glDisableVertexAttribArray(0);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
-    }while(glfwGetKey(window, GLFW_KEY_ESCAPE)!=GLFW_PRESS && glfwWindowShouldClose(window)==0);
+    }
 
+    while(glfwGetKey(window, GLFW_KEY_ESCAPE)!=GLFW_PRESS && glfwWindowShouldClose(window)==0);
 
     delete [] texture_data;
     ObjFile::clean_up(V,N, VT, FV, FN, FT);
+    glDeleteVertexArrays(1, &VertexArrayID);
+    glDeleteBuffers(1, &vertexbuffer);
+    glDeleteBuffers(1, &colorbuffer);
+
     return 0;
 }
