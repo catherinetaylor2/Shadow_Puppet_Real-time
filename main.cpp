16a4c2375e2600b74b7518743640fcc7af937802
Catherine <cat.taylor.taylor@btinternet.com>
2017-06-28 11:55:13 +0100

    Window opens.

--- main.cpp
+++ main.cpp
@@ -10,5 +10,37 @@
 #include <glm/gtx/transform.hpp>
 
 int main(){
+
+    if(!glfwInit()){ // initialize GLFW
+        std::cout<<"Error: failed to initialize GLFW \n";
+        return -1;
+    }
+    glfwWindowHint(GLFW_SAMPLES,4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window;
+    int width =1080, height = 720;
+    window = glfwCreateWindow(width, height, "Tutorial 01", NULL, NULL);
+    if(window==NULL){
+        std::cout<<"Error: failed to open window";
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glewExperimental = true;
+    if(glewInit()!=GLEW_OK){
+        std::cout<<"Error: failed to initialize GLEW \n";
+        return -1;
+    }
+     do{        
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    while(glfwGetKey(window, GLFW_KEY_ESCAPE)!=GLFW_PRESS && glfwWindowShouldClose(window)==0);
+
     return 0;
 }
