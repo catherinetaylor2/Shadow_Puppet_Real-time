6b24da11ecc837efda1cee93e6d02fd11f82094e
Catherine <cat.taylor.taylor@btinternet.com>
2017-07-05 18:07:04 +0100

    Began working with framebuffers.

--- main.cpp
+++ main.cpp
@@ -106,6 +106,8 @@ int main(){
     glGenBuffers(1, &colorbuffer);
     glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
     glBufferData(GL_ARRAY_BUFFER, 2*number_of_vertices*sizeof(float),  &VT[0], GL_STATIC_DRAW);
+
+   
 //---------------------------------------------------------------------------------------------------------------------------------------------------
 
     GLuint normalbuffer;
@@ -116,10 +118,46 @@ int main(){
     GLuint programID = LoadShaders("VertexShader.vertexshader", "FragmentShader.fragmentshader");
     GLuint MatrixID = glGetUniformLocation(programID, "MVP");
     GLuint LightID = glGetUniformLocation(programID, "LightPos");
+    
+ // FRAME BUFFER CODE: --------------------------------------------------------------------------------------------------------------------------
+
+unsigned int framebuffer;
+glGenFramebuffers(1, &framebuffer);
+glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);    
+// generate texture
+// unsigned int texColorBuffer;
+// glGenTextures(1, &texColorBuffer);
+// glBindTexture(GL_TEXTURE_2D, texColorBuffer);
+// glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+// glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
+// glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+// glBindTexture(GL_TEXTURE_2D, 0);
+
+// attach it to currently bound framebuffer object
+//glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texColorBuffer, 0);  
+unsigned int rbo;
+glGenRenderbuffers(1, &rbo);
+glBindRenderbuffer(GL_RENDERBUFFER, rbo); 
+glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, width, height);  
+glBindRenderbuffer(GL_RENDERBUFFER, 0);
+glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo);
+if(glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+	std::cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete! \n" ;
+glBindFramebuffer(GL_FRAMEBUFFER, 0); 
+
+ //-----------------------------------------------------------------------------------------------------------------------------------------------   
 
     do{
-        
-        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+        // bind to framebuffer and draw scene as we normally would to color texture 
+        // glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+        glEnable(GL_DEPTH_TEST); // enable depth testing (is disabled for rendering screen-space quad)
+
+        // // make sure we clear the framebuffer's content
+        // glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+         glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
         glUseProgram(programID);
         glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
         glUniform3fv(LightID, 1, &LightPos[0]);
@@ -163,6 +201,9 @@ int main(){
         glDrawElements(GL_TRIANGLES, 3*number_of_faces,  GL_UNSIGNED_INT,0);
         glDisableVertexAttribArray(0);
 
+          
+
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
@@ -177,6 +218,7 @@ int main(){
     glDeleteBuffers(1, &colorbuffer);
     glDeleteBuffers(1,&IBO);
     glDeleteBuffers(1, &normalbuffer);
+  //  glDeleteFramebuffers(1, &fbo);  
 
     return 0;
 }
